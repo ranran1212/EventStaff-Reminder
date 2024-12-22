f6105ded23efc8da8f5b04a054f53d5bc70659d6
ranran <32842733+ranran1212@users.noreply.github.com>
2024-12-23 02:59:28 +0900

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,9 +1,9 @@
 import os
 from twilio.rest import Client
 
-TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN  = os.environ.get('TWILIO_AUTH_TOKEN')
-TWILIO_PHONE_NUMBER = os.environ.get('TWILIO_PHONE_NUMBER')
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN  = os.environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.environ.get("TWILIO_PHONE_NUMBER")
 
 def send_sms(to_number: str, message_body: str):
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
